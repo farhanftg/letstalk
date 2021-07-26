e06acde6af16f83f22581f4df4abc4d437dfb3b3
farhan_hashmi <farhan.hashmi@girnarsoft.com>
2021-07-26 07:31:06 +0530

    changes done

--- config/config.js
+++ config/config.js
@@ -176,10 +176,10 @@ global.NODE_MODULE_PATH = BASE_DIR+'/node_modules/';
 global.ELK_FILE_PATH    = BASE_DIR+'/logs/vahan_scrapper.json';
 
 config.mysql = {};
-config.mysql.host = 'sg3plcpnl0183.prod.sin3.secureserver.net';
-config.mysql.user = 'letstalk'
-config.mysql.password = 'letstalk';
-config.mysql.database = 'letstalk';
+config.mysql.host = 'ec2-52-1-20-236.compute-1.amazonaws.com';
+config.mysql.user = 'rtzzxjnflzobwu'
+config.mysql.password = '8277debe05300f289b33f2ec7cb1e9982ad717bc722791fc7fba706f68b9828a';
+config.mysql.database = 'dertka4o19859i';
 
 config.mysqlTable = {};
 config.mysqlTable.users = 'users'
